f5cd5ed82af673100db34ef5920b8399a99419d1
NikaZelenkina <71321659+NikaZelenkina@users.noreply.github.com>
2022-01-18 08:44:43 +0700

    Add files via upload

---
+++ nika.js
@@ -0,0 +1,13 @@
+function show (nika)
+{ 
+ var elem = document.getElementById(nika);
+ if (elem)
+  elem.style.display="block";
+}
+
+function check()
+{
+if (calc.answer.value =="17")
+      alert("Правильно!");
+else  alert("НЕправильно!");
+}
